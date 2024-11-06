de8416c340233ed48ef6f9c153cfa9ab6f011c8d
ErikAlexandreKestring <erikalexandrekestring@gmail.com>
2024-11-05 21:06:45 -0300

    feat: supabase integration

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import '/login_screen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+
+void app() {
   runApp(MyApp());
 }
 
@@ -12,4 +14,12 @@ class MyApp extends StatelessWidget {
       home: LoginScreen(),
     );
   }
+}
+
+Future<void> main() async {
+  await Supabase.initialize(
+    url: 'https://ykppnlhznvhlfsgngzuy.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlrcHBubGh6bnZobGZzZ25nenV5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA4NDUzMjMsImV4cCI6MjA0NjQyMTMyM30.tNYUmX3pHwxE9YiYCdQdvX3fKVRiAF33yFCTpdfVVx4',
+  );
+  runApp(MyApp());
 }
